c227b579fa16d9b95a321e925c1340e0ab85bed0
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2026-03-05 17:27:31 +0200

    Update index.ts

--- NextJS-Frontend/careerics/services/index.ts
+++ NextJS-Frontend/careerics/services/index.ts
@@ -1,3 +1,4 @@
 export { authService } from "./auth.service";
 export { analyticsService } from "./analytics.service";
 export { interviewService } from "./interview.service";
+export { cvService } from "./cv.service";
